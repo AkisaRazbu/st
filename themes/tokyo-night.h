50cfc6bb8482feeae78cd53ef632fcebad0acaa8
akisa <akisa@tfwno.gf>
2021-12-25 18:04:04 +0100

    first commit

---
+++ themes/tokyo-night.h
@@ -0,0 +1,30 @@
+static const char *colorname[] = {
+
+/* 8 normal colors */
+  [0] = "#282a2e", /* black   */
+  [1] = "#f7758e", /* red     */
+  [2] = "#73daca", /* green   */
+  [3] = "#e0af67", /* yellow  */
+  [4] = "#7aa2f7", /* blue    */
+  [5] = "#bb99f7", /* magenta */
+  [6] = "#7ccfff", /* cyan    */
+  [7] = "#c0c9f5", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#f7758e", /* red     */
+  [10] = "#73daca", /* green   */
+  [11] = "#e0af67", /* yellow  */
+  [12] = "#7aa2f7", /* blue    */
+  [13] = "#bb99f7", /* magenta */
+  [14] = "#7ccfff", /* cyan    */
+  [15] = "#c0c9f5", /* white   */
+
+  /* special colors */
+  [256] = "#1a1b26", /* background */
+  [257] = "#c0c9f5", /* foreground */
+};
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 257;
